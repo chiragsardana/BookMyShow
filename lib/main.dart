4b8eb7d0c7ac10b7bd13a8f147ca71f576e1862a
chiragsardana <chiragsardana12@gmail.com>
2022-03-10 09:42:21 +0530

    BookMySHow Clone using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:book_my_show/screens/bookmyshow.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BookMyShow(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
